b24f621088b51699f4dc68a1610331585725e6e7
Ron Raney <rraney@kumc.edu>
2017-06-30 12:11:26 -0500

    Added a detail view and some more styles

--- demo/urls.py
+++ demo/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.demo_list, name='demo_list'),
+    url(r'^person/(?P<pk>\d+)/$', views.demo_detail, name='demo_detail'),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Person
 
 # Create your views here.
 def demo_list(request):
     people = Person.objects.order_by('unique_id')
     return render(request, 'demo/demo_list.html', {'people': people})
+
+def demo_detail(request, pk):
+    person = get_object_or_404(Person, pk=pk)
+    return render(request, 'demo/demo_detail.html', {'person': person})    
