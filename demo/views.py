68218e6ea81a7cc2f98b7cb3846587953f30812e
Ron Raney <rraney@kumc.edu>
2017-06-30 10:55:51 -0500

    Modified view, templates

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Person
 
 # Create your views here.
+def demo_list(request):
+    people = Person.objects.order_by('unique_id')
+    return render(request, 'demo/demo_list.html', {'people': people})
