98caf0e8d125241b6b3d11142be603ff8c264719
Ron Raney <rraney@kumc.edu>
2017-06-30 12:38:44 -0500

    Added a way to enter new people to database

--- demo/backups/urls.py
+++ demo/backups/urls.py
@@ -3,5 +3,4 @@ from . import views
 urlpatterns = [
     url(r'^$', views.demo_list, name='demo_list'),
     url(r'^person/(?P<pk>\d+)/$', views.demo_detail, name='demo_detail'),
-    url(r'^person/new/$', views.demo_new, name='demo_new'),
 ]
